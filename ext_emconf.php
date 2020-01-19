1d93290f8442802f83c99296954a92daeceb826d
Christian Schöpp <mail@dadadesign.de>
2020-01-19 15:47:32 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,6 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
+            'gridelements' => ''
         ],
         'conflicts' => [],
         'suggests' => [],
