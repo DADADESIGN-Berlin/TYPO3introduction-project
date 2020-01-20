0c1c5bc847ba6aea5b00f335ba9e3cac146e87dc
Christian Schöpp <mail@dadadesign.de>
2020-01-20 13:17:48 +0100

    Add static file from sys_template.php

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,4 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('dada_typo3introduction_project', 'Configuration/TypoScript', 'TYPO3introduction-project');
--- ext_tables.php
+++ ext_tables.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
+/* defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(
     function()
@@ -11,4 +11,4 @@ call_user_func(
 
 
     }
-);
+); */
